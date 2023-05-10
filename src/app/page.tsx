064715d1c75e8a0c566949c8794e4ed6c886a265
Rurik Pinheiro <rupinheiro@mgmresorts.com>
2023-05-10 12:44:25 -0300

    feat: update section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,10 +83,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            Custom files <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Lorem ipsum dolor
           </p>
         </a>
       </div>
